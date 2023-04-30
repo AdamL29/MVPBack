f7b855362697c75399e37ceebc5ecd56ca0ccf6f
Adam <adamlederer@live.ca>
2023-04-30 10:52:29 -0600

    added more notes for future work on front end.

--- endpoints/support.py
+++ endpoints/support.py
@@ -6,3 +6,5 @@ from check import check
 # Unaware of what should be here if you don't need to be signed in.
 # This is due to the factor of guests/visitors just being able to few pins.
 # Support should receive request and send to email address.
+# Need procedure made for selecting the email address the support goes to.
+# Need to sort out axios call to make pins show the proper details when clicked.
